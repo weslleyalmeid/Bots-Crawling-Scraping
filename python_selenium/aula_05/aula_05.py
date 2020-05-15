6e666f4b86839e3b2bada757c015173b966175b0
Weslley Almeida <weslley_fac@hotmail.com>
2020-05-15 09:59:53 -0300

    Selenium com Python #05 - Procurando e interagindo com elementos p.I

--- python_selenium/aula_05/aula_05.py
+++ python_selenium/aula_05/aula_05.py
@@ -5,34 +5,34 @@ from json import loads
 from time import sleep
 
 # todo -------------- exemplo 1 --------------------------
-# url = 'http://selenium.dunossauro.live/aula_05_a.html'
+url = 'http://selenium.dunossauro.live/aula_05_a.html'
 
-# firefox = Firefox()
+firefox = Firefox()
 
-# firefox.get(url)
+firefox.get(url)
 
-# div_py = firefox.find_element_by_id('python')
-# div_hk = firefox.find_element_by_id('haskell')
-# print(div_hk.text)
+div_py = firefox.find_element_by_id('python')
+div_hk = firefox.find_element_by_id('haskell')
+print(div_hk.text)
 
-# firefox.quit()
+firefox.quit()
 
 # todo -------------- exemplo 2 --------------------------
 
-# url = 'http://selenium.dunossauro.live/aula_05_b.html'
+url = 'http://selenium.dunossauro.live/aula_05_b.html'
 
-# firefox = Firefox()
+firefox = Firefox()
 
-# firefox.get(url)
+firefox.get(url)
 
-# topico = firefox.find_element_by_class_name('topico')
-# linguagens = firefox.find_elements_by_class_name('linguagens')
+topico = firefox.find_element_by_class_name('topico')
+linguagens = firefox.find_elements_by_class_name('linguagens')
 
-# for linguagen in linguagens:
-#     print(
-#         (linguagen.find_element_by_tag_name('h2').text,
-#          linguagen.find_element_by_tag_name('p').text)
-#     )
+for linguagen in linguagens:
+    print(
+        (linguagen.find_element_by_tag_name('h2').text,
+         linguagen.find_element_by_tag_name('p').text)
+    )
 
 
 # todo -------------- exemplo 3 --------------------------
